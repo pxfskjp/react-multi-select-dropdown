b5f3ecf1df7c13eab46426e32992a630ab27a691
coder7290 <topdev0729@gmail.com>
2021-09-01 21:18:30 +0900

    fix: display ibjects

--- src/components/SelectBoxComponent.js
+++ src/components/SelectBoxComponent.js
@@ -3,46 +3,62 @@ import { Multiselect } from "multiselect-react-dropdown";
 
 export default function SelectBoxComponent(props) {
   
-	const changedState = true;
-	const [plainArray, setPlainArray] = useState([]);
-	const [selectedValues, setSelectedValues] = useState([]);
+	const [subjectArray, setSubjectArray] = useState([]);
+	const [selectedSubjects, setSelectedSubjects] = useState([]);
 
 	useEffect(() => {
-		setPlainArray(["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]);
-	}, [changedState])
+		setSubjectArray(
+			[
+				{name: 'Option 1', id: 'option_1'}, 
+				{name: 'Option 2', id: 'option_2'}, 
+				{name: 'Option 3', id: 'option_3'}, 
+				{name: 'Option 4', id: 'option_4'}, 
+				{name: 'Option 5', id: 'option_5'}, 
+				{name: 'Option 6', id: 'option_6'}, 
+				{name: 'Option 7', id: 'option_7'}]
+		);
+	}, [subjectArray])
 
   const onSelect = (selectedList, selectedItem) => {
-    console.log(selectedList, selectedItem);
-		setSelectedValues(selectedList);
+    setSelectedSubjects(selectedList);
   }
 
   const onRemove = (selectedList, removedItem) => {
-    console.log(selectedList, removedItem);
-		setSelectedValues(selectedList)
+    setSelectedSubjects(selectedList);
   }
 
-	const makeSelectedOption = () => {
-		return <div>
-			asdfsadfsafsdf
-		</div>
-	}
+  const onSubjectClick = (selectedSubject) => {
+    console.log(selectedSubject);
+  }
 
 	return (
+    
 		<div className="App">
-			<div className="col-12 d-md-flex">
-				<div className="col-12 col-md-4 mt50 contents">
+      <div className="col-12 d-md-flex mt-3">
+        <br />
+        <div className="examples col-12 col-md-5 mt30">
+          <Multiselect 
+            options={subjectArray}
+            selectionLimit={5}
+            onSelect={(selectedList, selectedIte) => onSelect(selectedList, selectedIte)}
+            onRemove={(selectedList, removedItem) => onRemove(selectedList, removedItem)}
+            displayValue="name"
+          />
+        </div>
+      </div>
+
+			<div className="col-12 d-md-flex mt-3 mr-2 ml-2">
+				<div id="seleted_content" className="row contents">
 					{
-						makeSelectedOption()
+            selectedSubjects && selectedSubjects.map((item) => {
+              return <div key={item.id} className="d-md-flex mt-3">
+                <button type="button"
+                  onClick={() => onSubjectClick(item.id)}
+                  className="btn btn-primary m-1"  
+                >{item.name}</button>
+              </div>
+            })
 					}
-				</div>
-				<br />
-				<div className="examples col-12 col-md-5 mt30">
-					<Multiselect showArrow 
-						options={plainArray} 
-						isObject={false}
-						onSelect={(selectedList, selectedItem) => onSelect(selectedList, selectedItem)}
-						onRemove={(selectedList, removedItem) => onRemove(selectedList, removedItem)}
-							/>
 				</div>
 			</div>
 		</div>
