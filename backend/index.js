fd30111d4fa0f62bced8ccd93886d8132d347201
coder7290 <topdev0729@gmail.com>
2021-09-02 04:13:21 +0900

    add: add full dumy data

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
+const mongoose = ('mongoose');
+// const data = require('./data')
 
 const PORT = process.env.PORT || 3301;
 
 const app = express();
-
-
-
+// app.get('api/v1/categories', (req, res) => {
+//     return res.send(categories);
+// })
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 })
--- frontend/src/components/SelectBoxComponent.js
+++ frontend/src/components/SelectBoxComponent.js
@@ -1,74 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Multiselect } from "multiselect-react-dropdown";
+import data from '../data';
 
 export default function SelectBoxComponent(props) {
   
-  const allSubjectsArray = [
-    {subject: 'Category 1 Subject 1', id: 'category_1_subject_1', category_id: 'category_1'},
-    {subject: 'Category 1 Subject 2', id: 'category_1_subject_2', category_id: 'category_1'},
-    {subject: 'Category 1 Subject 3', id: 'category_1_subject_3', category_id: 'category_1'},
-    {subject: 'Category 1 Subject 4', id: 'category_1_subject_4', category_id: 'category_1'},
-    {subject: 'Category 1 Subject 5', id: 'category_1_subject_5', category_id: 'category_1'},
-    {subject: 'Category 1 Subject 6', id: 'category_1_subject_6', category_id: 'category_1'},
-    {subject: 'Category 2 Subject 1', id: 'category_2_subject_1', category_id: 'category_2'},
-    {subject: 'Category 2 Subject 2', id: 'category_2_subject_2', category_id: 'category_2'},
-    {subject: 'Category 2 Subject 3', id: 'category_2_subject_3', category_id: 'category_2'},
-    {subject: 'Category 2 Subject 4', id: 'category_2_subject_4', category_id: 'category_2'},
-    {subject: 'Category 2 Subject 5', id: 'category_2_subject_5', category_id: 'category_2'},
-    {subject: 'Category 2 Subject 6', id: 'category_2_subject_6', category_id: 'category_2'},
-    {subject: 'Category 2 Subject 7', id: 'category_2_subject_7', category_id: 'category_2'},
-    {subject: 'Category 3 Subject 1', id: 'category_3_subject_1', category_id: 'category_3'},
-    {subject: 'Category 3 Subject 2', id: 'category_3_subject_2', category_id: 'category_3'},
-    {subject: 'Category 3 Subject 3', id: 'category_3_subject_3', category_id: 'category_3'},
-    {subject: 'Category 4 Subject 1', id: 'category_4_subject_1', category_id: 'category_4'},
-    {subject: 'Category 4 Subject 2', id: 'category_4_subject_2', category_id: 'category_4'},
-    {subject: 'Category 4 Subject 3', id: 'category_4_subject_3', category_id: 'category_4'},
-    {subject: 'Category 4 Subject 4', id: 'category_4_subject_4', category_id: 'category_4'},
-    {subject: 'Category 4 Subject 5', id: 'category_4_subject_5', category_id: 'category_4'},
-    {subject: 'Category 4 Subject 6', id: 'category_4_subject_6', category_id: 'category_4'},
-    {subject: 'Category 5 Subject 1', id: 'category_5_subject_1', category_id: 'category_5'},
-    {subject: 'Category 5 Subject 2', id: 'category_5_subject_2', category_id: 'category_5'},
-    {subject: 'Category 5 Subject 3', id: 'category_5_subject_3', category_id: 'category_5'},
-    {subject: 'Category 5 Subject 4', id: 'category_5_subject_4', category_id: 'category_5'},
-    {subject: 'Category 5 Subject 5', id: 'category_5_subject_5', category_id: 'category_5'},
-    {subject: 'Category 6 Subject 1', id: 'category_6_subject_1', category_id: 'category_6'},
-    {subject: 'Category 6 Subject 2', id: 'category_6_subject_2', category_id: 'category_6'},
-    {subject: 'Category 6 Subject 3', id: 'category_6_subject_3', category_id: 'category_6'},
-    {subject: 'Category 7 Subject 1', id: 'category_7_subject_1', category_id: 'category_7'},
-    {subject: 'Category 7 Subject 2', id: 'category_7_subject_2', category_id: 'category_7'},
-    {subject: 'Category 7 Subject 3', id: 'category_7_subject_3', category_id: 'category_7'},
-    {subject: 'Category 7 Subject 4', id: 'category_7_subject_4', category_id: 'category_7'},
-  ];
+  const allSubjectsArray = data.subjects;
+  const allTagsArray = data.tags;
 
-  const allTagsArray = [
-    {tag: 'Subject 1 Tag 1', id: 'tag_1', subject_id: 'category_1_subject_1'},
-    {tag: 'Subject 1 Tag 2', id: 'tag_2', subject_id: 'category_1_subject_1'},
-    {tag: 'Subject 1 Tag 3', id: 'tag_3', subject_id: 'category_1_subject_1'},
-    {tag: 'Subject 1 Tag 4', id: 'tag_4', subject_id: 'category_1_subject_1'},
-    {tag: 'Subject 1 Tag 5', id: 'tag_5', subject_id: 'category_1_subject_1'},
-
-    {tag: 'Subject 2 Tag 1', id: 'tag_1', subject_id: 'category_1_subject_2'},
-    {tag: 'Subject 2 Tag 2', id: 'tag_2', subject_id: 'category_1_subject_2'},
-    {tag: 'Subject 2 Tag 3', id: 'tag_3', subject_id: 'category_1_subject_2'},
-    {tag: 'Subject 2 Tag 4', id: 'tag_4', subject_id: 'category_1_subject_2'},
-    {tag: 'Subject 2 Tag 5', id: 'tag_5', subject_id: 'category_1_subject_2'},
-    {tag: 'Subject 2 Tag 6', id: 'tag_6', subject_id: 'category_1_subject_2'},
-
-    
-    {tag: 'Subject 3 Tag 1', id: 'tag_1', subject_id: 'category_1_subject_3'},
-    {tag: 'Subject 3 Tag 2', id: 'tag_2', subject_id: 'category_1_subject_3'},
-    {tag: 'Subject 3 Tag 3', id: 'tag_3', subject_id: 'category_1_subject_3'},
-    {tag: 'Subject 3 Tag 4', id: 'tag_4', subject_id: 'category_1_subject_3'},
-    {tag: 'Subject 3 Tag 5', id: 'tag_5', subject_id: 'category_1_subject_3'},
-
-    
-    {tag: 'Subject 4 Tag 1', id: 'tag_1', subject_id: 'category_1_subject_4'},
-    {tag: 'Subject 4 Tag 2', id: 'tag_2', subject_id: 'category_1_subject_4'},
-    {tag: 'Subject 4 Tag 3', id: 'tag_3', subject_id: 'category_1_subject_4'},
-    {tag: 'Subject 4 Tag 4', id: 'tag_4', subject_id: 'category_1_subject_4'},
-    {tag: 'Subject 4 Tag 5', id: 'tag_5', subject_id: 'category_1_subject_4'},
-    
-  ];
 	const [categoryArray, setCategoryArray] = useState([]);
 	const [selectedCategories, setSelectedCategories] = useState([]);
   const [subjectsArray, setSubjectsArray] = useState([]);
@@ -77,16 +15,7 @@ export default function SelectBoxComponent(props) {
   const [selectedTags, setSelectedTags] = useState([]);
 
 	useEffect(() => {
-		setCategoryArray(
-			[
-				{category: 'Category 1', id: 'category_1'}, 
-				{category: 'Category 2', id: 'category_2'}, 
-				{category: 'Category 3', id: 'category_3'}, 
-				{category: 'Category 4', id: 'category_4'}, 
-				{category: 'Category 5', id: 'category_5'}, 
-				{category: 'Category 6', id: 'category_6'}, 
-				{category: 'Category 7', id: 'category_7'}]
-		);
+		setCategoryArray(data.categories);
 	}, [categoryArray, subjectsArray, selectedTags])
 
   const onSelect = (selectedList, selectedItem) => {
